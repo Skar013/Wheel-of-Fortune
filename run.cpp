6cd0c214343909ca9ecaa61cee601c066469e0a1
Skar013 <50510846+Skar013@users.noreply.github.com>
2022-02-24 05:28:31 +0400

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -1,5 +1,5 @@
 #include"head.h"
-#include<queue>
+
 using namespace std;
 
 
@@ -8,6 +8,7 @@ Content_::ContentControl wOb;
 wOb.read_content();
 wOb.split_content('\n');
 string s("");
+cout<<"Write word or words divided by space and use _ symbol for missing letters then press Enter\nFor example:he__o wo__d\n";
 while (getline(cin,s))
 {
    wOb<<s;
